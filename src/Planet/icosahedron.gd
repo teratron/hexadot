d7634fc7a2e1e6b3f4ec27dd1232e3d2c284b167
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2022-01-29 19:41:42 +0200

    get params

--- src/Planet/icosahedron.gd
+++ src/Planet/icosahedron.gd
@@ -1,89 +1,77 @@
 class_name Icosahedron, "res://assets/icosahedron.svg" extends Node
 
 
-const VERTICES:  int = 12  # вершины
-const EDGES:     int = 30  # рёбра
-const FACES:     int = 20  # грани
-const LATITUDES: int = 3   # широты
-const PARALLELS: int = 2   # паралели
-const POLES:     int = 2   # полюса
+enum POLES {NORTH, SOUTH} # полюса
 
-var vertices  = VERTICES
-var edges     = EDGES
-var faces     = FACES
-var latitudes = LATITUDES
-var parallels = PARALLELS
+export(int, 1, 10) var subdivision = 1  # кратность увеличения фрагментации икосаэдра
+export(float) var radius = 100          # радиус внешней, описывающей сферы
 
-export(int, 1, 10) var subdivision = 2  # кратность увеличения фрагментации икосаэдра
-export(float) var radius_outer = 100    # радиус внешней, описывающей сферы
+var vertices:  int = 12  # вершины
+var edges:     int = 30  # рёбра
+var faces:     int = 20  # грани
+var latitudes: int = 3   # широты
+var parallels: int = 2   # паралели
 
-var radius_inner: float = 100         # радиус внутренней, вписанной сферы
-var edge_length:  float = 1           # длина ребра
+var radius_inner: float  # радиус внутренней, вписанной сферы
+var edge_length:  float  # длина ребра
 
 
 func _init():
-	radius_outer = edge_length /  4 * sqrt(2 * (5 + sqrt(5)))
-	radius_inner = edge_length / (4 * sqrt(3)) * (3 + sqrt(5))
-	edge_length  = radius_outer * 4 / sqrt(2 * (5 + sqrt(5)))
-	
-	
-	print("Icosahedron")
-	print("Radius Outter:", radius_outer)
-	print("Radius Inner:",  radius_inner)
-	print("Edge Length:",   edge_length)
 	pass
 
 
 func _ready():
 	update_all_params(subdivision)
+	edge_length = get_length_edge(radius)
+	
+	#	radius_outer = edge_length /  4 * sqrt(2 * (5 + sqrt(5)))
+#	radius_inner = edge_length / (4 * sqrt(3)) * (3 + sqrt(5))
+#	print("Icosahedron")
+#	print("Radius Outter: ", radius_outer)
+#	print("Radius Inner: ",  radius_inner)
+	print("edge length: ", edge_length)
 	pass
 
 
-func update_all_params(multiple: int):
-	vertices = get_num_vertices(multiple)
-	edges    = get_num_edges(multiple)
-	faces    = get_num_faces(multiple)
+func update_all_params(_subdivision: int):
+	vertices  = get_num_vertices(_subdivision)
+	edges     = get_num_edges(_subdivision)
+	faces     = get_num_faces(_subdivision)
+	latitudes = get_num_latitudes(_subdivision)
+	parallels = get_num_parallels(_subdivision)
 	
-
-#		latitudes = LATITUDES
-#		parallels = PARALLELS
+	#for i in range(1, _subdivision + 1): print(i, " ",get_num_parallels(i))
+	print("%d vertices x%d" % [vertices, _subdivision])
+	print("%d edges x%d" % [edges, _subdivision])
+	print("%d faces x%d" % [faces, _subdivision])
+	print("%d latitudes x%d" % [latitudes, _subdivision])
+	print("%d parallels x%d" % [parallels, _subdivision])
 	return
 
 
-func get_num_vertices(multiple: int) -> int:
-	var vertex = VERTICES
-	var delta = 10
-	if multiple > 1:
-		for i in range(2, multiple + 1):
-			delta *= 4
-			print(i," ",pow(4, i - 1)*10," ", delta, " ", delta + 2, " ", FACES * delta / 20 + 2)
-		vertex = delta + 2
-	print("number of vertices %d for subdivision x%d" % [vertex, multiple])
-	return vertex
+func get_delta(_subdivision: int) -> int:
+	return int(pow(4, _subdivision - 1))
 
 
-func get_num_edges(multiple: int) -> int:
-	var edge = EDGES
-	var delta = 30
-	if multiple > 1:
-		for _i in range(2, multiple + 1):
-			delta *= 4
-			#print(i, " ", delta, " ", delta + EDGES)
-		edge = delta
-	print("number of edges %d for subdivision x%d" % [edge, multiple])
-	return edge
+func get_num_vertices(_subdivision: int) -> int:
+	return 10 * get_delta(_subdivision) + 2
 
 
-func get_num_faces(multiple: int) -> int:
-	var face = FACES
-	var delta = 1
-	if multiple > 1:
-		for _i in range(2, multiple + 1):
-			delta *= 4
-			#print(i, " ", FACES * delta)
-		face = FACES * delta
-	print("number of faces %d for subdivision x%d" % [face, multiple])
-	return face
+func get_num_edges(_subdivision: int) -> int:
+	return 30 * get_delta(_subdivision)
 
 
+func get_num_faces(_subdivision: int) -> int:
+	return 20 * get_delta(_subdivision)
 
+
+func get_num_latitudes(_subdivision: int) -> int:
+	return 3 * int(pow(2, _subdivision - 1))
+
+
+func get_num_parallels(_subdivision: int) -> int:
+	return get_num_latitudes(_subdivision) - 1
+
+
+func get_length_edge(_radius) -> float:
+	return _radius * 4 / sqrt(2 * (5 + sqrt(5)))
