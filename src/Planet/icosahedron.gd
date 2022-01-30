b3a807bd35f8276c0cd5cfc12cbe9f67f70c292a
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2022-01-30 14:09:17 +0200

    classes

--- src/Planet/icosahedron.gd
+++ src/Planet/icosahedron.gd
@@ -1,23 +1,39 @@
 class_name Icosahedron, "res://assets/icosahedron.svg" extends Node
 
 
+const PENTAGON_ANGLE = 72
+
 export(int, 1, 10) var subdivision = 1  # кратность увеличения фрагментации икосаэдра
 export(float) var radius = 100          # радиус внешней, описывающей сферы
 
-var vertices:  int = 12  # вершины
-var edges:     int = 30  # рёбра
-var faces:     int = 20  # грани
-var latitudes: int = 3   # широты
-var parallels: Array = [5, 5] # паралели
+#var vertices:  int = 12       # вершины
+#var edges:     int = 30       # рёбра
+#var faces:     int = 20       # грани
+#var latitudes: int = 3        # широты
+#var parallels: Array = [5, 5] # паралели
 
-var latitude_angle: float
-var radius_inner:   float    # радиус внутренней, вписанной сферы
-var edge_length:    float    # длина ребра
-
+#var parallel_angle: float
+var radius_inner:   float     # радиус внутренней, вписанной сферы
+#var edge_length:    float     # длина ребра
+var icosahedron = []
 var coords = []
+
+class Figure:
+	var subdivision:    int
+	var vertices:       int       # вершины
+	var edges:          int       # рёбра
+	var faces:          int       # грани
+	var latitudes:      int       # широты
+	var edge_length:    float     # длина ребра
+	var parallel_angle: float
+	
+	func _init(lat = 0, lon = 0):
+		latitude  = lat
+		longitude = lon
 
 
 class Vertex:
+	var subdivision: int
 	var latitude:  float
 	var longitude: float
 	var x: float
@@ -38,9 +54,11 @@ func _ready():
 	edges       = get_num_edges(subdivision)
 	faces       = get_num_faces(subdivision)
 	latitudes   = get_num_latitudes(subdivision)
-	parallels   = get_num_parallels(parallels, subdivision)
+	#parallels   = get_num_parallels(parallels, subdivision)
 	edge_length = get_length_edge(radius)
-	latitude_angle = 180 / float(latitudes)
+	parallel_angle = 180 / float(latitudes)
+	
+	
 	
 	var n = vertices - 1
 	coords.resize(vertices)
@@ -55,7 +73,6 @@ func _ready():
 	#print(coords[0].longitude)
 	
 	
-	
 #	radius_outer = edge_length /  4 * sqrt(2 * (5 + sqrt(5)))
 #	radius_inner = edge_length / (4 * sqrt(3)) * (3 + sqrt(5))
 #	print("Icosahedron")
@@ -68,7 +85,7 @@ func _ready():
 	print("%d latitudes x%d" % [latitudes, subdivision])
 	#print("%a parallels x%d" % [parallels, subdivision])
 	print("edge length: ", edge_length)
-	print("latitude angle: ", latitude_angle)
+	print("latitude angle: ", parallel_angle)
 	pass
 
 
@@ -92,13 +109,26 @@ func get_num_latitudes(_subdivision: int) -> int:
 	return 3 * int(pow(2, _subdivision - 1))
 
 
-func get_num_parallels(_parallels: Array, _subdivision: int) -> Array:
-	var n = get_num_latitudes(_subdivision) - 1
-	_parallels.resize(n)
-	print(len(_parallels))
-	##for i in n:
-		#print(i)#_parallels[]
-	return _parallels
+#func get_num_parallels(_parallels: Array, _subdivision: int) -> Array:
+#	var delta = 5
+#	var n = get_num_latitudes(_subdivision) - 1
+#	_parallels.resize(n)
+#	#print(len(_parallels))
+#	#print(ceil(n / 2.0
+#	for i in ceil(n / 2.0):
+#		_parallels[i] = delta
+#		var j = n - i - 1
+#		if i < j:
+#			_parallels[j] = delta
+#		delta = i * 5
+#		# 2 5  / 5 10   10   10 5
+#		# 3 11 / 5 10 15 20 20   20   20 20 15 10 5
+#		# 4 23 / 5 10 15 20 30 30 40 40 40 40 40   40   40 40 40 40 40 30 30 20 15 10 5
+#		#        1  2  3  4  6  6  8  8  8  8  8
+#
+#	print(_parallels)
+#
+#	return _parallels
 
 
 func get_length_edge(_radius) -> float:
