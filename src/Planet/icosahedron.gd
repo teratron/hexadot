771c185b755e2a02455670e96515d76804524943
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2022-02-01 07:21:07 +0200

    vertex

--- src/Planet/icosahedron.gd
+++ src/Planet/icosahedron.gd
@@ -16,8 +16,8 @@ var radius_inner:   float          # радиус внутренней, впис
 var edge_length:    float          # длина ребра
 
 #var icosahedron: Array
-var coordinates: Array
 var surface:     Array
+var coordinates: Array
 #var parallels = [1, 5, 5, 1]
 
 
@@ -56,19 +56,16 @@ class Figure:
 
 
 class Face:
-	var id: int
-	var corners: Array
-	var faces_near: Array
-	
-	var edges:       int = 3   # рёбра
-	var faces:       int = 4   # грани
-	var edge_length: float     # длина ребра
-	#var coordinates: Array
+	var subdivision:   int
+	var id:            int
+	var faces_near_id: Array
+	var vertices_id:   Array
 
 
 class Vertex:
 	var subdivision: int
-	var face_id:   Array
+	var id:          int
+	var faces_id:    Array
 	
 	var latitude:  float
 	var longitude: float
@@ -80,40 +77,68 @@ class Vertex:
 		latitude  = lat
 		longitude = lon
 
+#0  0 9
+#1  4 1  -3
+#2  8 3  -5
+#3 12 5  -7
+#4 16 7  -9
 
 func _init():
 	parallel_angle = 180 / float(latitudes)
 	edge_length    = radius * 4 / sqrt(2 * (5 + sqrt(5)))
 	radius_inner   = edge_length / (4 * sqrt(3)) * (3 + sqrt(5))
 	
-	
-	
-	var n = faces - 1
 	surface.resize(faces)
-	for i in faces:
-		surface[i] = Face.new()
-		
-		var u: int # индекс следующей плоскости
-		var v: int # индекс предыдующей плоскости
-		var w: int # индекс плоскости граничащей с основанием
-		
-		if i == 0:
-			v = PENTAGON - 1
-		else:
-			v = i - 1
-		
-		if i == n:
-			u = i - PENTAGON + 1
-		else:
-			u = i + 1
-		
-		w = i + PENTAGON
-		#if i >= NORTH_HEMISPHERE
-		
-		prints(i, u, v, w)
-		surface[i].faces_near = [u, v, w]
+#	for i in faces:
+#		surface[i] = Face.new()
+#		surface[i].subdivision = subdivision
+#		surface[i].id = i
+#		#surface[i].faces_near_id = set_faces_near_id(i)
+#		surface[i].faces_near_id = set_alt_faces_near_id()
+#		surface[i].vertices_id = set_vertices_id(i)
+#		prints(i, surface[i].faces_near_id)
 	
-	#print(surface)
+	var a: int
+	var b: int
+	var c: int
+	var x: int
+	var y: int
+	var z: int
+	var i = 0
+	for j in 5:
+		for k in 4:
+			surface[i] = Face.new()
+			surface[i].subdivision = subdivision
+			surface[i].id = i
+			
+			match k:
+				0:
+					a = i + 1
+					if j == 4: b = i - 16; else: b = i + 4
+					if j == 0: c = i + 16; else: c = i - 4
+					
+					x = 0
+					if j == 0: y = j + 9; else: y = 2 * j - 1
+				1:
+					a = i - 1
+					if j == 0: b = i + 17; else: b = i - 3
+					c = i + 1
+				2:
+					a = i + 1
+					if j == 4: b = i - 17; else: b = i + 3
+					c = i - 1
+				3:
+					a = i - 1
+					if j == 0: b = i + 16; else: b = i - 4
+					if j == 4: c = i - 16; else: c = i + 4
+					
+					z = 0
+			
+			surface[i].faces_near_id = [a, b, c]
+			surface[i].vertices_id = [x, y, z]
+			
+			prints(i, surface[i].faces_near_id)
+			i += 1
 	
 #	var i = 0
 #	coordinates.resize(vertices)
@@ -122,13 +147,8 @@ func _init():
 #			prints(i, j, k)
 #			coordinates[i] = Vertex.new()
 #			coordinates[i].subdivision = subdivision
-#			coordinates[i].face_id = []
+#			coordinates[i].faces_id = []
 #			i += 1
-	
-#	coordinates.resize(faces)
-#	for i in range(0, faces):
-#		coordinates[i] = Vertex.new()
-#		print(i)
 	
 	
 #	var n = vertices - 1
@@ -160,7 +180,6 @@ func _ready():
 #		icosahedron[i] = Figure.new(i + 1, radius)
 
 
-
 #	var n = vertices - 1
 #	coordinates.resize(vertices)
 #	coordinates[0] = Vertex.new(0, 0)   # северный полюс
@@ -188,6 +207,115 @@ func _ready():
 #	print("edge length: ", edge_length)
 #	print("latitude angle: ", parallel_angle)
 	pass
+
+
+#func set_faces_near_id(index: int) -> Array:
+#	var u: int # индекс следующей плоскости
+#	var v: int # индекс предыдующей плоскости
+#	var w: int # индекс плоскости граничащей с основанием
+#
+#	# северный и южный полюс
+#	if index < 5 || index > 14:
+#		if index == 4 || index == 19:
+#			u = index - 4
+#		else:
+#			u = index + 1
+#		if index == 0 || index == 15:
+#			v = index + 4
+#		else:
+#			v = index - 1
+#		if index < 5:
+#			w = index + 5
+#		if index > 14:
+#			w = index - 5
+#
+#	# экватор
+#	else:
+#		if index > 4 && index < 10:
+#			u = index + 5
+#			if index == 5:
+#				v = index + 9
+#			else:
+#				v = index + 4
+#			w = index - 5
+#		if index > 9 && index < 15:
+#			if index == 14:
+#				u = index - 9
+#			else:
+#				u = index - 4
+#			v = index - 5
+#			w = index + 5
+#
+#	return [u, v, w]
+
+
+#func set_alt_faces_near_id() -> Array:
+#	var a: int
+#	var b: int
+#	var c: int
+#	var i = 0
+#
+#	for j in 5:
+#		for k in 4:
+#			match k:
+#				0:
+#					a = i + 1
+#					if j == 4: b = i - 16; else: b = i + 4
+#					if j == 0: c = i + 16; else: c = i - 4
+#				1:
+#					a = i - 1
+#					if j == 0: b = i + 17; else: b = i - 3
+#					c = i + 1
+#				2:
+#					a = i + 1
+#					if j == 4: b = i - 17; else: b = i + 3
+#					c = i - 1
+#				3:
+#					a = i - 1
+#					if j == 0: b = i + 16; else: b = i - 4
+#					if j == 4: c = i - 16; else: c = i + 4
+#			i += 1
+#
+#	return [a, b, c]
+
+#func set_vertices_id(index: int) -> Array:
+#	var u: int # индекс следующей плоскости
+#	var v: int # индекс предыдующей плоскости
+#	var w: int # индекс плоскости граничащей с основанием
+#
+#	if index < 5:
+#		u = 0
+#		if index == 0:
+#			v = 5
+#		else:
+#			v = index
+#		w = index + 1
+#		if index > 4 && index < 10:
+#			u = index + 5
+#			if index == 5:
+#				v = index + 9
+#			else:
+#				v = index + 4
+#			w = index - 5
+#		if index > 9 && index < 15:
+#			if index == 14:
+#				u = index - 9
+#			else:
+#				u = index - 4
+#			v = index - 5
+#			w = index + 5
+#
+#	return [u, v, w]
+
+#0 0 5 1
+#1 0 1 2
+#2 0 2 3
+#3 0 3 4
+#4 0 4 5
+
+
+
+
 
 
 #func get_delta(_subdivision: int) -> int:
